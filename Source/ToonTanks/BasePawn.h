9a47573623b8691e269816a715ad439fd2e839cb
David Acosta <david.acosta.ing@gmail.com>
2024-12-20 17:32:02 -0500

    Setting up Enemy turrets, and player tank. Creating Blueprints from BasePawn C++ class

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -14,19 +14,29 @@ class TOONTANKS_API ABasePawn : public APawn
 public:
 	// Sets default values for this pawn's properties
 	ABasePawn();
-
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	
 private:
-	UPROPERTY()
+	// VisibleAnywhere, EditAnywhere, values can be modified in their blueprints and the instances of those blueprints
+	// VisibleInstanceOnly, EditInstanceOnly, values can be modifie or visible only in the instances of the blueprint
+	// VisibleDefaultOnly, EditDefaultsOnly, values can be visible or modified only on the blueprint, not the instances
+	// BlueprintReadWrite, BlueprintReadOnly, values can be modified or read-only on blueprints event graph
+	UPROPERTY(BlueprintReadWrite, meta=(AllowPrivateAccess="true"), VisibleAnywhere, Category="Components")
 	class UCapsuleComponent* CapsuleComp; // Forward declaration to avoid unnecessary headers UCapsule is already included in APawn class
-	UPROPERTY()
+	UPROPERTY(BlueprintReadWrite, meta=(AllowPrivateAccess="true"), VisibleAnywhere, Category="Components")
 	UStaticMeshComponent* BaseMesh; // header or forward declaration are not needed for UStaticMeshComponent since it comes with Actor class
-	UPROPERTY()
+	UPROPERTY(BlueprintReadWrite, meta=(AllowPrivateAccess="true"), VisibleAnywhere, Category="Components")
 	UStaticMeshComponent* TurretMesh;
-	UPROPERTY()
+	UPROPERTY(BlueprintReadWrite, meta=(AllowPrivateAccess="true"), VisibleAnywhere, Category="Components")
 	USceneComponent* ProjectileSpawnPoint;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+	
 	
 public:	
 	// Called every frame
