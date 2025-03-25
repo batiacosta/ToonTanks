af6a41de9903ebe6c41ab603021f239defeaee4d
David Acosta <david.acosta.ing@gmail.com>
2025-03-25 12:51:55 -0500

    Adding C++ Tank to BP_PawnTank

---
+++ Source/ToonTanks/Tank.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Tank.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATank : public ABasePawn
+{
+	GENERATED_BODY()
+public:
+	ATank();
+
+private:
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Components")
+	class UCameraComponent* Camera;
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"), Category = "Components")
+	class USpringArmComponent* SpringArm;
+
+protected:
+	
+};
